b271553e648b934a7a183828e78466aea35be521
DESKTOP-KQA1RBO\raffo <raffocinematic@gmail.com>
2023-06-23 11:37:00 +0200

    First and last commit ;)

---
+++ Pong/src/Ball.java
@@ -0,0 +1,51 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
+
+public class Ball extends Rectangle {
+	
+	Random random;
+	int xVelocity;
+	int yVelocity;
+	int initialSpeed = 3;
+	
+	Ball(int x, int y, int width, int height) {
+		super(x,y,width,height);
+		random = new Random();
+		int randomXDirection = random.nextInt(2);
+		if(randomXDirection == 0)
+			randomXDirection--;
+		setXDirection(randomXDirection*initialSpeed);
+		
+		int randomYDirection = random.nextInt(2);
+		if(randomYDirection == 0)
+			randomYDirection--;
+		setYDirection(randomYDirection*initialSpeed);
+		
+	}
+	//la palla si muove sull'asse X e Y in maniera casuale quando creata
+	public void setXDirection(int randomXDirection) {
+		xVelocity = randomXDirection;
+	}
+
+	public void setYDirection(int randomYDirection) {
+		yVelocity = randomYDirection;
+		
+	}
+	
+	public void move() {
+		x += xVelocity;
+		y += yVelocity;
+		
+	}
+	
+	public void draw(Graphics g) {
+		g.setColor(Color.white);
+		g.fillOval(x,y,height,width);
+		
+	}
+	
+	
+	
+}
